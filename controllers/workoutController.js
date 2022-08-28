16eed984ae5ceec5a8afc8c2f347e11b5d1b6a8b
joycethh <joycethh@outlook.com>
2022-08-27 21:07:36 -0500

    destructure data

--- controllers/workoutController.js
+++ controllers/workoutController.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 //get all workouts
 const getWorkouts = async (req, res) => {
   try {
-    const workouts = await Workout.find({}).sort({ createdAt: -1 });
+    const workouts = await Workout.find().sort({ createdAt: -1 });
 
     res.status(200).json(workouts);
   } catch (error) {
