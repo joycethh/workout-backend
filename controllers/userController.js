c167bcb07357596c62b2b7ce617ce21af6cf6024
joycethh <joycethh@outlook.com>
2022-09-12 16:15:26 -0500

    add login user logic

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
 
 const createToken = (id) => {
   return jwt.sign({ id }, process.env.SECREAT, { expiresIn: "3d" });
@@ -8,7 +9,29 @@ const createToken = (id) => {
 
 //login user
 const loginUser = async (req, res) => {
-  res.json({ mssg: "sign in users" });
+  try {
+    const { email, password } = req.body;
+    //search db to see if there is such user
+    const existedUser = await User.findOne({ email });
+    //user not existed
+    if (!existedUser) {
+      res.status(404).json({ mssg: `User doesn't exist` });
+    }
+    //user exist &&  then check password
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      existedUser.password
+    );
+    //wrong password
+    if (!isPasswordCorrect) {
+      res.status(400).json({ mssg: "Invalid passwords" });
+    }
+    //correct password, then assign a token to the user
+    const token = createToken(existedUser._id);
+    res.status(200).json({ email, token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 //signup user
