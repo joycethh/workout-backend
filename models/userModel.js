024ad49400b40f34f2f66d55672d786169c0f343
joycethh <joycethh@outlook.com>
2022-09-12 15:01:14 -0500

    use validator to sign up user

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const validator = require("validator");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
@@ -16,6 +17,16 @@ const userSchema = new Schema({
 
 //static method
 userSchema.statics.signup = async function (email, password) {
+  if (!email || !password) {
+    throw Error("All fields needs to be filled");
+  }
+  if (!validator.isEmail(email)) {
+    throw Error("Email is not valid");
+  }
+  if (!validator.isStrongPassword(password)) {
+    throw Error("Password is not strong enough");
+  }
+
   const existEmail = await this.findOne({ email });
   if (existEmail) {
     throw Error("Email is already in use");
