11fc8458607d90d63254a153fac0be0d70f56ad0
joycethh <joycethh@outlook.com>
2022-09-12 15:55:50 -0500

    sign up user and send signing token

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,15 +1,23 @@
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
+
+const createToken = (id) => {
+  return jwt.sign({ id }, process.env.SECREAT, { expiresIn: "3d" });
+};
 
 //login user
 const loginUser = async (req, res) => {
   res.json({ mssg: "sign in users" });
 };
 
+//signup user
 const signupUser = async (req, res) => {
   const { email, password } = req.body;
   try {
     const user = await User.signup(email, password);
-    res.status(200).json({ email, user });
+    const token = createToken(user._id);
+    res.status(200).json({ email, token });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
